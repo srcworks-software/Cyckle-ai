ae194ee6b36f29d94b353d284a804f959b29df14
vaultdweller-2287 <zane.apatoff@gmail.com>
2025-02-06 00:28:01 +0000

    Update and rename install.sh to dependency-fixer.sh

--- install/dependency-fixer.sh
+++ install/dependency-fixer.sh
@@ -1,7 +1,9 @@
 sudo apt install -y python3 python3-pip
+sudo apt install -y python3.13-dev
 pip install gpt4all --break-system-packages
 clear
 echo "==PyLLM UNATTENDED INSTALLATION=="
 python3 -m wizard.py
 echo "==INSTALLATION COMPLETE=="
 echo "Type 'python3 -m main.py' to use PyLLM"
+echo "or execute the main executable."
