35c1b6636cb78f50939aa59eeddad265cc62e8f2
vaultdweller-2287 <zane.apatoff@gmail.com>
2025-02-06 00:30:32 +0000

    Update dependency-fixer.sh

--- install/dependency-fixer.sh
+++ install/dependency-fixer.sh
@@ -1,5 +1,6 @@
 sudo apt install -y python3 python3-pip
 sudo apt install -y python3.13-dev
+sudo apt install -y gcc make build-essential
 pip install gpt4all --break-system-packages
 clear
 echo "==PyLLM UNATTENDED INSTALLATION=="
