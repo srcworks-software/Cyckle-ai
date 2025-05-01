c871c9338d3539df0a8a1ddc8e7156d32db6a4a9
vaultdweller-2287 <zane.apatoff@gmail.com>
2025-05-01 21:11:24 +0000

    add wget to dependencies

--- cyckle-chat/dependency-fixer.sh
+++ cyckle-chat/dependency-fixer.sh
@@ -1,6 +1,10 @@
 sudo apt install -y python3-minimal python3-pip
 sudo apt install -y python3.13-dev
 sudo apt install -y gcc make build-essential
+sudo apt install -y wget
+cd models
+wget https://huggingface.co/bartowski/Phi-3.5-mini-instruct-GGUF/resolve/main/Phi-3.5-mini-instruct-IQ3_XS.gguf
+cd -
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -r requirements.txt
